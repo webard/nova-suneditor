626cb8ccd7f92f398fb8973e4d402bc85afac890
Bartłomiej Gajda <b.gajda@webard.me>
2024-07-15 23:31:22 +0200

    build

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,9 +1,9 @@
-import IndexField from './components/IndexField'
+// import IndexField from './components/IndexField'
 import DetailField from './components/DetailField'
 import FormField from './components/FormField'
 
 Nova.booting((app, store) => {
-  app.component('index-suneditor', IndexField)
+  // app.component('index-suneditor', IndexField)
   app.component('detail-suneditor', DetailField)
   app.component('form-suneditor', FormField)
 })
